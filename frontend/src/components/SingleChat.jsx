1458b64108dbb96d0f3ee52e105a735bcbb9f2df
Divy Mav <divymav5@gmail.com>
2024-11-16 23:39:28 +0530

    Uploading all images

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = process.env.REACT_APP_BACKEND_API || "http://localhost:5000";
+const ENDPOINT = process.env.REACT_APP_BACKEND_API || "https://voluble-zcnj.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
